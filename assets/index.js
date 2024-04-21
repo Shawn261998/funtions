54c32516876bad4cacf0fb29527dce7b40784011
Shawn261998 <mehts400@newschool.edu>
2024-04-21 13:08:56 -0400

    added an assets folder

---
+++ assets/index.js
@@ -0,0 +1,39 @@
+const generatePrompt = () => {
+  const industryFilter = document.getElementById('industryFilter').value;
+  let filteredPrompts = [];
+
+  if (industryFilter === 'all') {
+    filteredPrompts = prompts; // Shows all the prompts
+  } else {
+    filteredPrompts = prompts.filter(prompt => prompt.industry === industryFilter);
+  }
+
+  const randomIndex = Math.floor(Math.random() * filteredPrompts.length);
+  const randomPrompt = filteredPrompts[randomIndex];
+
+  const promptDisplay = document.getElementById('promptDisplay');
+
+  promptDisplay.innerHTML = 
+    `
+      <div class="background" style="background-color: ${randomPrompt['background-color']}">
+        <h3 class="design-brief">${randomPrompt['design-brief']}</h3>
+        <p class="deliverable"> Deliverable: ${randomPrompt.deliverable}</p>
+      </div>
+    `
+  ;
+};
+
+// Generate prompt button.
+document.getElementById('generateBtn').addEventListener('click', generatePrompt);
+
+// Fetching JSON data and storing it in prompts array
+fetch('assests/data.json')
+  .then(response => response.json())
+  .then(data => {
+    prompts = data; // Store the fetched data in the prompts array
+    generatePrompt(); // Call generatePrompt function to display a random prompt
+  })
+  .catch(error => console.error('Error fetching data:', error));
+
+// Generate prompt when the page loads
+window.onload = generatePrompt;
